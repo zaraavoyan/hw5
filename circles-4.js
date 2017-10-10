60647e88d9b7a6283ae2219662e8af6db1ea12bd
zaraavoyan <31899165+zaraavoyan@users.noreply.github.com>
2017-10-09 23:21:38 -0700

    Create circles-4.js

---
+++ circles-4.js
@@ -0,0 +1,47 @@
+var circles = [2, 6, 6, 3, 70, 58, 25, 24];
+// var i = 0;
+// var h = 30;
+var i,h;
+var y = 300;
+
+// var x= 50+i*70;
+
+function setup() {
+	background(255);
+	frameRate(9);
+	createCanvas (600, 600);
+	var i = 0;
+	var h = 30;
+	
+}
+
+function draw() {
+	
+	circles.forEach(drawCircles);
+	circles.forEach(translateCircles);
+	
+} 
+
+function drawCircles() {
+	 background(255);
+   for (var i = 0; i < 8; i += 1) {
+  colorMode(HSB);
+		 noStroke();
+	
+		fill(255,100,100);
+	ellipse(50+i*70, y, circles[i], circles[i]);
+		 
+	 }
+}
+
+function translateCircles() {
+// circle.x += circle.vx; // circle.x = circle.x + circle.vx
+//   circle.y += circle.vy;
+// 	if (circle.x > 100) {
+// 	circle.x = 95}
+	
+ y +=10; 
+	if (y > 350) {
+	y = 300}
+}
+
